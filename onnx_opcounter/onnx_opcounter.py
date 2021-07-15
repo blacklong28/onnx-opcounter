fix bug for output name and overflow error

onnx_opcounter/onnx_opcounter.py
@@ -67,9 +67,10 @@ def calculate_macs(model: onnx.ModelProto) -> int:
         for output in node.output:
             if output in graph_outputs:
                 return output
-        return node.name
+        # return node.name
+        return node.output[0]
 
-    output_name_mapping = {node.name: get_mapping_for_node(node, graph_outputs) for node in onnx_nodes}
+    output_name_mapping = {node.output[0]: get_mapping_for_node(node, graph_outputs) for node in onnx_nodes}
     output_mapping = {}
 
     for name in output_name_mapping:
@@ -104,7 +105,7 @@ def calculate_macs(model: onnx.ModelProto) -> int:
 
         in_channels = input_shape[1]
 
-        return np.prod(output_shape) * (in_channels // group * kernel_ops + bias_ops)
+        return np.int64(np.prod(output_shape,dtype=np.int64) * (in_channels // group * kernel_ops + bias_ops))
 
     def gemm_macs(node, input_shape, output_shape, attrs):
         return np.prod(input_shape) * np.prod(output_shape)
@@ -141,11 +142,19 @@ def calculate_macs(model: onnx.ModelProto) -> int:
         'Upsample': upsample_macs,
     }
 
-    macs = 0
+    macs = np.int64(0)
+    
     for node in onnx_nodes:
-        node_output_shape = output_shapes[node.name]
-        node_input_shape = output_shapes[node.input[0]]
-        macs += mac_calculators[node.op_type](
-            node, node_input_shape, node_output_shape, onnx_node_attributes_to_dict(node.attribute)
-        )
+        try:
+            node_output_shape = output_shapes[node.output[0]]
+            node_input_shape = output_shapes[node.input[0]]
+        except:
+            continue
+        if node.op_type in mac_calculators:
+            new = mac_calculators[node.op_type](
+                node, node_input_shape, node_output_shape, onnx_node_attributes_to_dict(node.attribute)
+            )
+            macs += new
+        else:
+            print("Skip the statistical operator:{},op_name:{}".format(node.op_type,node.name))
     return macs